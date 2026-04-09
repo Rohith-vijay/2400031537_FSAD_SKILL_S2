42ff6ecfaab542f2f0b24795d25cb995e4e6bc84
kl2400031537 <2400031537@kluniversity.in>
2026-04-09 11:39:21 +0530

    Delete skill12 directory

--- skill12/Skill-6/src/main/java/com/klu/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
--- skill12/Skill-9/src/main/java/com/klu/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
